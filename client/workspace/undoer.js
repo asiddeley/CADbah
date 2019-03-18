26b6ede29db736f214c1429eaec8f90aafd18523
Andrew Siddeley <andrew.siddeley@norr.com>
2019-03-18 19:08:42 -0400

    work on undo

--- client/workspace/undoer.js
+++ client/workspace/undoer.js
@@ -4,16 +4,18 @@ Copyright (c) 2019 Andrew Siddeley
 MIT License
 ***/
 
-// This module is written as like a single instantiated class, a singleton.
+// This module is written as a single instantiated class, a singleton.
 
 // PRIVATE STATIC
 var CAD;
 var ulist=[];
 var ui=-1;
-var Undo=function(){
-	this.set=function(name, val){this[name]=val;};	
+var Undo=function(operation){
+	this.operation=operation; //name of undo operation
+	this.set=function(valname, val){this.values[valname]=val;};	
 	this.setPro=function(fn){this.pro=fn;};
-	this.setRetro=function(fn){this.retro=fn;};
+	this.setRetro=function(fn){this.retro=fn;};	
+	this.values={};
 };
 
 // PUBLIC
@@ -26,24 +28,28 @@ $.extend(exports,
 	require("./tool")
 );
 
+exports.name="undoer";
+
 exports.activate=function(workspace){
 	CAD=workspace.CAD; 
 	return exports;
 };
 
 exports.create=function(name){
-	/* Check index against undo list. If less it means actions have been undone by goback() so need to purge ulist from index onward to allow list to continue with a revised branch of actions. */
+	/* Check index against undo list. If less it means actions have been undone by goback() so need to purge ulist from index onward to allow list to continue with a revised branch of operations */
 	while (ui+1<ulist.length){ulist.pop();}
 	var u=new Undo(name);
 	ulist.push(u); ui=ulist.length-1;
 	return u;
 };
 
-exports.name="undoer";
-
-exports.setScene=function(scene){
-	
-};
+exports.dir=function(){
+	var htm="<ol>";
+	for (var i=0 to ui){
+		htm+="<li>"+i.operation+"</li>";
+	}
+	return htm+"</ol>";
+}
 
 exports.goforth=function(){
 	//execute retro function and move index back
@@ -51,8 +57,20 @@ exports.goforth=function(){
 	else {CAD.msg("Nothing to redo");}	
 };
 
-exports.goback=function(){
+exports.goback=function(toIndex){
+	toIndex=toIndex||ui-1;
 	//execute retro function and move index back
-	if (ui >= 0){ulist[ui].retro();ui--;}
-	else {CAD.msg("Nothing to undo");}
+	while (ui >= 0 && toIndex<ui){
+		CAD.msg("undo &gt;"+ulist[ui].operation);
+		try{ulist[ui].retro();ui--;}
+		catch(e){CAD.msg("error"+e.msg);}
+	}
+	if (ui<0){CAD.msg("Nothing to undo");}
+
 };
+
+exports.setScene=function(scene){};
+
+
+
+
